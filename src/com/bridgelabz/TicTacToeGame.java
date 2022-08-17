1d62cc63728f3968cff811e93056f0bb6065d6df
adithya89780 <adithyasachin123@gmail.com>
2022-08-17 13:48:59 +0530

    Added: user make a move

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,9 +1,10 @@
 package com.bridgelabz;
 import java.util.Scanner;
-
+//uc-1
 public class TicTacToeGame {
-    static char[][] board = {{'1', '2', '3'}, {'4', '5', '6'},{'7', '8', '9'}};
-
+    static char[][] board = {{' ', ' ', ' '}, {' ', ' ', ' '},{' ', ' ', ' '}};
+    public static char chooseLetter;
+    //uc-2
     public static void chooseLetter() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Choose a letter ( X or O ) : ");
@@ -12,6 +13,7 @@ public class TicTacToeGame {
         System.out.println("computer choose :");
         System.out.println(computerLetter);
     }
+    //uc-3
     public static void printBoard(char[][] board) {
         System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
         System.out.println("-+-+-");
@@ -19,9 +21,50 @@ public class TicTacToeGame {
         System.out.println("-+-+-");
         System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
     }
+    //uc-4
+    private static void playerTurn(String userInput) {
+        switch (userInput){
+            case "1" :
+                board[0][0] = 'X';
+                break;
+            case "2" :
+                board[0][1] = 'X';
+                break;
+            case "3" :
+                board[0][2] = 'X';
+                break;
+            case "4" :
+                board[1][0] = 'X';
+                break;
+            case "5" :
+                board[1][1] = 'X';
+                break;
+            case "6" :
+                board[1][2] = 'X';
+                break;
+            case "7" :
+                board[2][0] = 'X';
+                break;
+            case "8" :
+                board[2][1] = 'X';
+                break;
+            case "9" :
+                board[2][2] = 'X';
+                break;
+            default:
+                System.out.println("error");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe Game");
         chooseLetter();
+        printBoard(board);
+        //uc-4
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" choose a number (1 to 9) : ");
+        String userInput = scanner.nextLine();
+        System.out.println(userInput);
+        playerTurn(userInput);
         printBoard(board);
     }
 }
